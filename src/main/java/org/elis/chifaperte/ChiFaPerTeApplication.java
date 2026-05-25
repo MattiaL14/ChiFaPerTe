c9e77c8960c0bb5301b2a9570d5ea0c261054849
MattiaL14 <mattial.jobs@gmail.com>
2026-05-25 15:48:16 +0200

    Initial commit

---
+++ src/main/java/org/elis/chifaperte/ChiFaPerTeApplication.java
@@ -0,0 +1,13 @@
+package org.elis.chifaperte;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ChiFaPerTeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ChiFaPerTeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/org/elis/chifaperte/ChiFaPerTeApplicationTests.java
@@ -0,0 +1,13 @@
+package org.elis.chifaperte;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChiFaPerTeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
